96ccc5210612b8188e176d4f6193bdb4d4c9521e
vagrant <vagrant@localhost.localdomain>
2016-11-08 16:47:25 +0000

    Fixed a bug where we couldn't specify a relative path for template and less files. Fix router link to overview screen

--- client/app/claimfile/appendix-tabs/appendix-tabs.component.ts
+++ client/app/claimfile/appendix-tabs/appendix-tabs.component.ts
@@ -2,12 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'prop-appendix-tabs',
-    /**
-     * @todo : check why Webpack doesn't accept relative path : './claimfile-tabs.component.html'
-     * whereas it accepts it on module bootstrap component
-     */
-    templateUrl : 'app/claimfile/appendix-tabs/appendix-tabs.component.html',
-    styleUrls : ['app/claimfile/appendix-tabs/appendix-tabs.component.less']
+    templateUrl: 'appendix-tabs.component.html',
+    styleUrls: ['appendix-tabs.component.less']
 })
 export class AppendixTabsComponent {
 

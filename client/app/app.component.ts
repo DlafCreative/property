1d7fd72dbbe1a1fddf92fd46f7d146a9c7544e0f
tino.nuon@gmail.com <tino.nuon@gmail.com>
2016-11-29 19:17:53 +0000

    [Layout] Hide header on login page (in progress)

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component }  from '@angular/core';
+
+import { Router, ActivatedRoute }     from '@angular/router';
 
 @Component({
   selector: 'prop-app',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        let currentRoute$ = this.route;
+    }
+}

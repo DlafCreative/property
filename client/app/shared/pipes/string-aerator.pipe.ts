b63d60988ba2b6feef3f8af6c121b384e88c7d73
tino.nuon@gmail.com <tino.nuon@gmail.com>
2016-12-02 19:17:41 +0000

    [Datadisplay] Extends StringAerator pipe to handle phone number in the future

--- client/app/shared/pipes/string-aerator.pipe.ts
+++ client/app/shared/pipes/string-aerator.pipe.ts
@@ -7,10 +7,35 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'stringAerator'})
 export class StringAeratorPipe implements PipeTransform {
 
-    transform(string): string {
-        string = string.replace(/\s+/g, ''); 
-        let output = `${string.substr(0,4)} ${string.substr(4,3)} ${string.substr(7,3)} ${string.substr(10,3)}`;
+    static WAN = 'WAN';
+    static PHONE_NUMBER = 'phoneNumber_fr';
+
+    transform(chain: string, type: string = 'WAN'): string {
+        let output = null;
+        if (chain) {
+            chain = chain.replace(/\s+/g, ''); 
+
+            if (type === StringAeratorPipe.WAN) {
+                output; 
+            }
+
+            switch (type) {
+                case StringAeratorPipe.WAN: 
+                    return this._transformToWan(chain);
+                case StringAeratorPipe.PHONE_NUMBER:
+                    return this._transformToPhoneNumber(chain);
+
+            }
+        }
         return output;
+    }
+
+    _transformToWan(chain: string): string {
+        return `${chain.substr(0,4)} ${chain.substr(4,3)} ${chain.substr(7,3)} ${chain.substr(10,3)}`;
+    }
+
+    _transformToPhoneNumber(chain: string): string {
+        return `${chain.substr(0,2)} ${chain.substr(2,2)} ${chain.substr(4,2)} ${chain.substr(6,2)}`;
     }
 
 }

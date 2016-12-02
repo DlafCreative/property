aaae0233a4c6c5b505cb647e9cf9a5fc029da798
tino.nuon@gmail.com <tino.nuon@gmail.com>
2016-12-02 15:32:37 +0000

    [CSS] Appendix : Move CSS into component scoped styles

--- client/app/claimfile/appendix-tabs/appendix-tabs.component.ts
+++ client/app/claimfile/appendix-tabs/appendix-tabs.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'prop-appendix-tabs',
     templateUrl: 'appendix-tabs.component.html',
-    styleUrls: ['appendix-tabs.component.less']
+    styleUrls: ['appendix-tabs.component.less'],
+    encapsulation: ViewEncapsulation.Native
 })
 export class AppendixTabsComponent {
     
